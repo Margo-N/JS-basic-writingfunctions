8a93e6560470bb869d7fd40c5c0315cea844509c
Margo N <margoneff3@gmail.com>
2024-10-19 13:59:22 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var addTogether = function (num1, num2) {
+  return num1 + num2;
+};
+
+alert(addTogether(36, -2.88));
+console.log(addTogether(13, 72));
+var lunchForTwo = addTogether(23.56, 18.99);
+console.log(lunchForTwo);
